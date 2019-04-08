b9e0385337e97c09fb795f6b619b2d71910f5fa9
Samuel Soper <ssoper@madisoncollege.edu>
2019-04-08 18:06:26 -0500

    Adding some bindings to the title to make it larger and change color.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'quiz-editor';
+
+  myWidth = 250;
+
+  makeImageLarger() {
+    // make the image 30% larger each time you click on it
+    this.myWidth *=1.3
+  }
+
+  // Read-only/getter property
+  get titleColor() {
+    return this.myWidth > 400 ? "red" : "blue";
+  }
 }
