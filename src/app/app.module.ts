6bf21928290973c2581da0bf1e9ca415ddb38f2c
Samuel Soper <ssoper@madisoncollege.edu>
2019-04-15 20:33:01 -0500

    Implemented a bootstrap component in the quiz editor Other section...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule
     , FormsModule
     , HttpClientModule
+    , NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
